304a23b13f778fb7821b86f9bf4f938ffa0e98c4
septiaheskaa <heskaseptia@gmail.com>
2024-05-21 15:01:42 +0700

    menambah file

---
+++ jsx.d.ts
@@ -0,0 +1,9 @@
+// jsx.d.ts
+import 'vue';
+
+declare module 'vue' {
+  interface JSX {
+    Element: any; // Update this as needed based on your project
+    ElementAttributesProperty: any; // Update this as needed based on your project
+  }
+}
